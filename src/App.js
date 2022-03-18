91ef0470536fb38e63bd0d70a91b7db4f723d7a7
51p50x <51plog50@gmail.com>
2022-03-18 13:30:28 -0600

    formik-1: created form using formik, with the useFormik hook we handle the submit of the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import {useFormik} from 'formik'
 
 function App() {
+  const formik = useFormik({
+    initialValues:{
+      name: '',
+      lastname: '',
+      email: '',
+    },
+    onSubmit: values => console.log(values)
+  })
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+      <form onSubmit={formik.handleSubmit}>
+        <label>Name</label>
+        <input
+          name='name'
+          type='text'
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label>Lastname</label>
+        <input
+          name='lastname'
+          type='text'
+          onChange={formik.handleChange}
+          value={formik.values.lastname}
+        />
+        <label>Email</label>
+        <input
+          name='email'
+          type='email'
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <button type='submit'>Enviar</button>
+      </form>
   );
 }
 
