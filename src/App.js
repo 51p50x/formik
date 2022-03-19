e781a83c247b52dc100eea4f77ffb5718fb90f24
51p50x <51plog50@gmail.com>
2022-03-19 17:19:58 -0600

    formik-2: added validate method for name and lastname field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import {useFormik} from 'formik'
 
+const validate = (values) =>{
+  const errors = {}
+  
+  if(!values.name){
+    errors.name = 'Name is required'
+  } else if (values.name.length < 5){
+    errors.name = 'Name is too short'
+  }
+  
+  if(!values.lastname){
+    errors.lastname = 'Lastname is required'
+  } else if (values.lastname.length < 5){
+    errors.lastname = 'Lastname is too short'
+  }
+  
+  return errors
+}
+
 function App() {
   const formik = useFormik({
     initialValues:{
@@ -7,6 +25,7 @@ function App() {
       lastname: '',
       email: '',
     },
+    validate,
     onSubmit: values => console.log(values)
   })
   return (
@@ -18,6 +37,7 @@ function App() {
           onChange={formik.handleChange}
           value={formik.values.name}
         />
+        {formik.errors.name? <div>{formik.errors.name}</div> : null}
         <label>Lastname</label>
         <input
           name='lastname'
@@ -25,6 +45,7 @@ function App() {
           onChange={formik.handleChange}
           value={formik.values.lastname}
         />
+        {formik.errors.lastname? <div>{formik.errors.lastname}</div> : null}
         <label>Email</label>
         <input
           name='email'
