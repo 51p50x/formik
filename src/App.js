5caea4a9629a4e3f4a5ac6e7e52fae2ab87ba0d5
51p50x <51plog50@gmail.com>
2022-03-20 17:55:34 -0600

    formik-11: created radiobutton custom component with formik

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Formik, Field, Form, ErrorMessage} from 'formik';
 import Checkbox from './Components/Checkbox';
 import TextInput from './Components/TextInput'
 import Select from './Components/Select';
+import Radio from './Components/Radio';
 
 const validate = values => {
   const errors = {};
@@ -18,31 +19,40 @@ const validate = values => {
     errors.lastname = 'Lastname is too short';
   }
 
+  if(!values.radio){
+    errors.radio = 'Required'
+  }
+
   return errors;
 };
 
 function App () {
   return (
     <Formik
-      initialValues={{name: '', lastname: '', email: ''}}
+      initialValues={{name: '', lastname: '', email: '', select:'default', accept:false, radio:''}}
       validate={validate}
       onSubmit={values => console.log (values)}
     >
       <Form>
         <TextInput name = 'name' label = 'Name' />
         <br/>
+        <TextInput name = 'lastname' label = 'Lastname' />
+        <br/>
+        <TextInput name = 'email' label = 'Email' />
+        <br/>
         <Select label='select waifu' name = 'select'>
             <option value='default'>Default option</option>
             <option value='happy waifu'>happy waifu</option>
         </Select>
         <br/>
-        <TextInput name = 'lastname' label = 'Lastname' />
-        <br/>
-        <TextInput name = 'email' label = 'Email' />
-        <br/>
         <Checkbox name='accept'>
             Accept terms and conditions
         </Checkbox>
+        <br/>
+        <Radio name ='radio' label = 'waifu 1' value='waifu1'/>
+        <Radio name ='radio' label = 'waifu 2' value='waifu2'/>
+        <Radio name ='radio' label = 'waifu 3' value='waifu3'/>
+        <ErrorMessage name ='radio' />
         <br/>
         <button type="submit">Sent</button>
       </Form>
